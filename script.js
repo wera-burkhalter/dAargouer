cae824f40ee9426bb24180cdc91e8f7ea0e9ae88
JanUrech <janic.urech@stud.fhgr.ch>
2024-05-07 11:38:07 +0200

    Filter failed

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const app = document.querySelector("#aare-app");
+const suchBox = document.querySelector('#suchBox');
 // let bestellurl = "https://aareguru.existenz.ch/v2018/cities";
 
 // async function bestellen(bestellurl) {
@@ -14,8 +15,6 @@ const app = document.querySelector("#aare-app");
 
 // console.log(bestellen(bestellurl));
 // console.log(bestellen(bestellurl).promise.Array[0].aare);
-
-
 
 let cities = ["brienz", "interlaken", "thun", "bern", "hagneck", "biel", "olten", "brugg"];
 let currenturl = "https://aareguru.existenz.ch/v2018/current";
@@ -67,3 +66,53 @@ anfragen("https://aareguru.existenz.ch/v2018/current?city=").then(results => { /
 
     });
 });
+
+async function search(stadtFilter) {
+    let searchValue = suchBox.value;
+    let url = `https://aareguru.existenz.ch/v2018/current?city=${searchValue}`;
+    app.innerHTML = '';
+    let aareStadt = await fetchData(url);
+    console.log(aareStadt);
+    console.log(aareStadt.aare.location);
+    }
+    
+suchBox.addEventListener('input', search);
+
+function createItem(stadtFilter){
+let item = document.createElement('div');
+item.innerHTML += `<article class="infoBox">
+<img src="Images/${location}.jpg" alt="${location}" class="infoBoxImg">
+<h3 class="infoBoxTitel">${location}</h3>
+<dl class="infoBoxTabelle">
+
+    <dt>wassertemparatur</dt>
+    <dd>${temperature}</dd>
+    <dt>lufttemparatur</dt>
+    <dd>${tt}</dd>
+    <dt>badeempfehlung</dt>
+    <dd>${temperature_text}</dd>
+</div>
+</article>`;
+app.appendChild(item);
+}
+
+// function createItem(cocktail) {
+//     let item = document.createElement('div');
+//     item.classList.add('drink');
+//     item.innerHTML = `<h2>${cocktail.strDrink}</h2> 
+//     <img src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}">
+//     <p>${cocktail.strInstructions}</p>`;
+    
+//     cocktailApp.appendChild(item);
+// }
+
+async function fetchData(url) {
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
