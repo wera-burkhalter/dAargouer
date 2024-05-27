b0c72b48b5a838e84b53a7c08737796383638db7
JanUrech <janic.urech@stud.fhgr.ch>
2024-05-27 15:10:44 +0200

    About us siite finalisiert

--- aboutus.js
+++ aboutus.js
@@ -3,22 +3,22 @@ const app = document.querySelector("#teammembers");
 // Define the objects
 const Janic = {
     name: "Janic",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.",
-    Image: "images/janic.jpg",
+    description: "Als ambitonierte Aarebädeler und Warmduscher isch em Janic dr Thermometer es bsundrigs Herzensprojekt gsii. Solang dä ned mindistens Grüen wird, haltet er sich vom Wasser fern. Usserdem hed er üsi Team siite gstaltet. Ganz nach em Motto «Wieso eifach wes au Kompliziert gaht» hed er dass ganze miteme Array im Javascript gstaltet, statts eifach im CSS zmache. ",
+    Image: "images/Team/janic.jpg",
 };
 
 const Nicole = {
     name: "Nicole",
     description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.",
 
-    Image: "images/nicole.jpg",
+    Image: "images/Team/nicole.jpg",
 };
 
 const Wera = {
     name: "Wera",
     description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.",
 
-    Image: "images/wera.jpg",
+    Image: "images/Team/wera.jpg",
 };
 
 // Create the array and include the objects
@@ -36,8 +36,8 @@ Team.forEach((teamMember, index) => {
      let reverseClass = index % 2 === 1 ? 'Reverse' : '';
 
     app.innerHTML += `     <article class="member${reverseClass}">
-    <div class="memberText">
-        <h3> ${name}</h3>
+    <div class="memberText${reverseClass}">
+        <h2> ${name}</h2>
         <p class="memberDescription${reverseClass}"> ${description}</p>
     </div>
     <img src="${image}" alt="Portrait von ${name}" class="memberImage${reverseClass}">
