d218ff89f23d8ce6a02b0a49cdd491b73bc300ea
NicoleRau <nicole.rau@stud.fhgr.ch>
2024-05-31 08:21:46 +0200

    Anpassungen Über uns Seite
    Ich habe textliche Anpassungen bei der Über uns Seite gemacht.

--- aboutus.js
+++ aboutus.js
@@ -9,7 +9,7 @@ const Janic = {
 
 const Nicole = {
     name: "Nicole",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.",
+    description: "Us regumässigi Aareguru-Nutzerin isch ar Nicole schnäu klar gsi was si für neh API wott ihbinge. Da sie aus ehemaligi Orientierigsläuferin gärn Charte het, het si ar Gruppe sCharte-Design vorgschlage. Si het sich oh näre ganz intensiv mitm Design, sprich unger angerem dSchriftarte und dFarbgäbig, gkümmeret. S'CSS isch auso ihres Speziaugebiet gsi.",
 
     Image: "images/Team/nicole.jpg",
 };
