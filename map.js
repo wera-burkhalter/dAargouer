d0c4ef11b69e0ecdf66713153c51081649cc4f90
wera-burkhalter <wera.burkhalter@stud.fhgr.ch>
2024-06-02 13:48:09 +0200

    Map Seite fertiggestellt
    - Kurzbeschreibungen hinzugefügt
    - Scrollen nach rechts und links blockiert

--- map.js
+++ map.js
@@ -1,19 +1,19 @@
 // Selektiere die DOM-Elemente
-const thermometerFill = document.querySelector('#thermometerFill');
-const app = document.querySelector("#aare-app");
-const thermometerZahl = document.querySelector('#thermometerZahl');
+const thermometerFill = document.querySelector('#thermometerFill'); // Thermometer-Füllung
+const app = document.querySelector("#aare-app"); // Hauptbereich der App
+const thermometerZahl = document.querySelector('#thermometerZahl'); // Temperaturanzeige im Thermometer
 
 // Definiere die Städte und die URL für die Anfragen
 let cities = ["brienz", "interlaken", "thun", "bern", "hagneck", "biel", "olten", "brugg"];
-let currenturl = "https://aareguru.existenz.ch/v2018/current";
+let currenturl = "https://aareguru.existenz.ch/v2018/current"; // Basis-URL für die Anfragen
 
 // Funktion zur Durchführung der Anfragen
 async function anfragen(url) {
     try {
-        const anfrage = await fetch(url);
-        return await anfrage.json();
+        const anfrage = await fetch(url); // API-Anfrage senden
+        return await anfrage.json(); // Warte auf die Antwort und konvertiere sie in JSON
     } catch (e) {
-        console.error(e);
+        console.error(e); // Konsolenausgabe des Fehlers, falls die Anfrage fehlschlägt
         return [];
     }
 }
@@ -21,16 +21,19 @@ async function anfragen(url) {
 // THERMOMETER
 // Funktion zur Aktualisierung der Temperatur für eine bestimmte Stadt
 function updateTemperature(city) {
-    const url = `https://aareguru.existenz.ch/v2018/current?city=${city}`;
+    const url = `https://aareguru.existenz.ch/v2018/current?city=${city}`; // URL für die Anfrage
 
+    // Anfrage senden und die Temperatur aktualisieren
     anfragen(url).then(results => {
-        console.log(results.aare.temperature);
-        thermometerFill.innerHTML = '';
-        thermometerZahl.innerHTML = '';
-        const temperature = results.aare.temperature;
-        thermometerZahl.innerHTML += `<div> ${temperature} </div>`;
-        let thermoLiquid = temperature * 2.5;
-        thermometerFill.style.height = `${thermoLiquid}%`;
+        console.log(results.aare.temperature); // Konsolenausgabe der Temperatur
+        thermometerFill.innerHTML = ''; // Themometer-Füllung leeren
+        thermometerZahl.innerHTML = ''; // Temperaturanzeige leeren
+        const temperature = results.aare.temperature; // Temperatur aus den Ergebnissen holen
+        thermometerZahl.innerHTML += `<div> ${temperature} </div>`; // Temperaturanzeige aktualisieren
+        let thermoLiquid = temperature * 2.5; // Berechne die Höhe der Thermometer-Füllung basierend auf der Temperatur
+        thermometerFill.style.height = `${thermoLiquid}%`; // Aktualisiere die Höhe der Thermometer-Füllung
+        
+        // Farbverlauf für die Thermometer-Füllung basierend auf der Temperatur ändern
         if (temperature < 10) {
             thermometerFill.style.backgroundImage = "linear-gradient(to top, #3a55a5, #8dd7f2)";
         }
@@ -53,17 +56,20 @@ function updateTemperature(city) {
 }
 
 // INFOBOX
+//Funktion zur Anzeige der InfoBox für eine bestimmte Stadt
 function infoBox(city) {
-    const url = `https://aareguru.existenz.ch/v2018/current?city=${city}`;
+    const url = `https://aareguru.existenz.ch/v2018/current?city=${city}`; // URL für die Anfrage
 
+    // Anfrage senden und die InfoBox aktualisieren
     anfragen(url).then(results => {
-        console.log(results.aare.location);
-        app.innerHTML = ''; // Clear previous infoBox
-        let location = results.aare.location;
-        let temperature = results.aare.temperature;
-        let temperature_text = results.aare.temperature_text;
-        let tt = results.weather.current.tt;
+        console.log(results.aare.location); // Standort in der Konsole ausgeben
+        app.innerHTML = ''; // vorherige InfoBox leeren
+        let location = results.aare.location; //Standort aus den Ergebnissen holen
+        let temperature = results.aare.temperature; // Temperatur aus den Ergebnissen holen
+        let temperature_text = results.aare.temperature_text; // Temperaturtext aus den Ergebnissen holen
+        let tt = results.weather.current.tt; // aktuelle Lufttemperatur aus den Ergebnissen holen
 
+        // InfoBox-Inhalt aktualisieren
         app.innerHTML += `
             <article class="infoBox">
                 <button class="close-btn" onclick="closeInfoBox()">✖</button>
@@ -83,94 +89,99 @@ function infoBox(city) {
     });
 }
 
-// Funktion zum Schließen der InfoBox
+// Funktion zum Schliessen der InfoBox
 function closeInfoBox() {
-    app.innerHTML = ''; // Clear the infoBox content
+    app.innerHTML = ''; // Inhalt der InfoBox leeren
 }
 
 // Funktion zur Anpassung der Kartenkoordinaten entsprechend dem Skalierungsfaktor
 function adjustMapCoords(scaleFactor) {
-    const areas = document.querySelectorAll('map[name="chmap"] area');
+    const areas = document.querySelectorAll('map[name="chmap"] area'); // Alle Kartenbereiche selektieren
     areas.forEach(area => {
-        let coords = area.getAttribute('coords').split(',').map(Number);
-        let scaledCoords = coords.map(coord => Math.round(coord * scaleFactor));
-        area.setAttribute('coords', scaledCoords.join(','));
+        let coords = area.getAttribute('coords').split(',').map(Number); // Koordinaten des Bereichs holen und in Zahlen umwandeln
+        let scaledCoords = coords.map(coord => Math.round(coord * scaleFactor)); // Koordinaten basierend auf dem Skalierungsfaktor anpassen
+        area.setAttribute('coords', scaledCoords.join(',')); // Angepasste Koordinaten setzen
     });
 }
 
 // Funktion zur Anpassung der Bootkoordinaten basierend auf der Kartenkoordinaten
 function adjustBoatPosition() {
-    const mapWidth = document.getElementById('schweizerKarte').offsetWidth;
-    const mapHeight = document.getElementById('schweizerKarte').offsetHeight;
-    const boat = document.getElementById('boat');
+    const mapWidth = document.getElementById('schweizerKarte').offsetWidth; // Breite der Karte holen
+    const mapHeight = document.getElementById('schweizerKarte').offsetHeight; // Höhe der Karte holen
+    const boat = document.getElementById('boat'); // Boot-Element selektieren
 
-    const xOffset = mapWidth * 0.48; // Beispielpositionierung: 50% der Kartenbreite
-    const yOffset = mapHeight * 0.52; // Beispielpositionierung: 40% der Kartenhöhe
+    const xOffset = mapWidth * 0.48; // X-Positionierung: 48% der Kartenbreite
+    const yOffset = mapHeight * 0.52; // Y-Positionierung: 52% der Kartenhöhe
 
-    boat.style.left = `${xOffset}px`;
-    boat.style.top = `${yOffset}px`;
+    boat.style.left = `${xOffset}px`; // Linke Position des Bootes setzen
+    boat.style.top = `${yOffset}px`; // Obere Position des Bootes setzen
 }
 
 // Initiale Anpassung der Bootposition und bei Größenänderung des Fensters
 document.addEventListener('DOMContentLoaded', () => {
-    adjustBoatPosition();
-    window.addEventListener('resize', adjustBoatPosition);
-    const originalWidth = 800; // Original width of the map
-    const container = document.getElementById('containerSchweizerkarte');
-    const newWidth = container.offsetWidth; // New width of the map
-    const scaleFactor = newWidth / originalWidth; // Scale factor based on the new width
+    adjustBoatPosition(); // initiale Anpassung
+    window.addEventListener('resize', adjustBoatPosition); // Anpassung bei Grössenänderung des Fensters
+    const originalWidth = 800; // Originalbreite der Karte
+    const container = document.getElementById('containerSchweizerkarte'); // Kartencontainer
+    const newWidth = container.offsetWidth; // Neue Breite der Karte
+    const scaleFactor = newWidth / originalWidth; // Skalierungsfaktor basierend auf der neuen Breite berechnen
 
-    adjustMapCoords(scaleFactor); // Initial adjustment
+    adjustMapCoords(scaleFactor); // initiale Kartenkoordinaten anpassen
 
     window.addEventListener('resize', () => {
-        const newScaleFactor = container.offsetWidth / originalWidth;
-        adjustMapCoords(newScaleFactor);
+        const newScaleFactor = container.offsetWidth / originalWidth; // neuer Skalierungsfaktor basierend auf der neuen Breite berechnen
+        adjustMapCoords(newScaleFactor); // Anpassung der Kartenkoordinaten bei Grössenänderung
     });
 });
 
 // Funktion zum Bewegen des Bootes zu einem bestimmten Punkt
 function moveBoatTo(point) {
-    const [x, y] = point.coords.split(',').map(Number);
-    const boatWidth = boat.offsetWidth;
-    const boatHeight = boat.offsetHeight;
-    const offsetX = boatWidth / 2;
-    const offsetY = boatHeight / 2;
-    boat.style.left = `${x - offsetX}px`;
-    boat.style.top = `${y - offsetY}px`;
+    const [x, y] = point.coords.split(',').map(Number); // Koordinaten des Punktes holen und in Zahlen umwandeln
+    const boatWidth = boat.offsetWidth; // Breite des Bootes holen
+    const boatHeight = boat.offsetHeight; // Höhe des Bootes holen
+    const offsetX = boatWidth / 2; // horizontaler Versatz für die Zentrierung
+    const offsetY = boatHeight / 2; // vertikaler Versatz für die Zentrierung
+    boat.style.left = `${x - offsetX}px`; // linke Position des Bootes setzen
+    boat.style.top = `${y - offsetY}px`; // obere Position des Bootes setzen
 }
 
+
 // SCHWEIZERKARTE MIT PUNKTEN
-const map = document.querySelectorAll('#chmap area');
+const map = document.querySelectorAll('#chmap area'); // Alle Punkte auf der Karte selektieren
 map.forEach((point) => {
     point.addEventListener('click', (event) => {
-        const city = point.dataset.city;
-        updateTemperature(city); // Aktualisiere Temperatur beim Klick auf einen Punkt
-        infoBox(city); // Zeige Informationen für die Stadt an
-        moveBoatTo(point); // Bewege das Boot zu dem Punkt
+        const city = point.dataset.city; // Stadt aus dem Datensatz des Punktes holen
+        updateTemperature(city); // Temperatur für die Stadt aktualisieren
+        infoBox(city); // Informationen zur Stadt anzeigen
+        moveBoatTo(point); // Boot zum Punkt bewegen
     });
 });
 
-// BOOT
-const boat = document.createElement('img');
-boat.src = 'Images/AareBootBlau.png';
-boat.id = 'boat';
-boat.style.position = 'absolute';
-document.getElementById('containerSchweizerkarte').appendChild(boat);
 
-document.getElementById('abBoot').addEventListener('click', function() {
-    this.classList.add('animate');
-    const targetElement = document.getElementById('schweizerKarte');
+// BOOT AUF KARTE
+const boat = document.createElement('img'); // Boot-Element erstellen
+boat.src = 'Images/AareBootBlau.png'; // Bild des Bootes setzen
+boat.id = 'boat'; // ID des Bootes setzen
+boat.style.position = 'absolute'; // Positionierung des Bootes auf absolut setzen
+document.getElementById('containerSchweizerkarte').appendChild(boat); // Boot zum Container hinzufügen
+
+// Scroll-Animation beim Klick auf den Button
+document.getElementById('abBoot').addEventListener('click', function() { 
+    this.classList.add('animate'); // Animation hinzufügen
+    const targetElement = document.getElementById('schweizerKarte'); // Ziel-Element selektieren
     if (targetElement) {
-        scrollToElement(targetElement, 5000);
+        scrollToElement(targetElement, 5000); // Scrollen zum Ziel-Element über 5 Sekunden
     }
 });
 
+// Funktion zum sanften Scrollen zu einem bestimmten Element
 function scrollToElement(element, duration) {
-    const targetPosition = element.getBoundingClientRect().top + window.scrollY - 170;
-    const startPosition = window.scrollY;
-    const distance = targetPosition - startPosition;
-    const startTime = performance.now();
+    const targetPosition = element.getBoundingClientRect().top + window.scrollY - 170; // Zielposition berechnen
+    const startPosition = window.scrollY; // Startposition holen
+    const distance = targetPosition - startPosition; // Distanz zum Ziel berechnen
+    const startTime = performance.now(); // Startzeit holen
 
+    // Funktion für einen sanften Übergang
     function easeInOutQuad(time, start, change, duration) {
         time /= duration / 2;
         if (time < 1) return change / 2 * time * time + start;
@@ -178,15 +189,16 @@ function scrollToElement(element, duration) {
         return -change / 2 * (time * (time - 2) - 1) + start;
     }
 
+    // Funktion für die Scroll-Schleife
     function scrollLoop(currentTime) {
-        const elapsedTime = currentTime - startTime;
-        window.scrollTo(0, easeInOutQuad(elapsedTime, startPosition, distance, duration));
+        const elapsedTime = currentTime - startTime; // vergangene Zeit berechnen
+        window.scrollTo(0, easeInOutQuad(elapsedTime, startPosition, distance, duration)); // Scroll-Position anpassen
         if (elapsedTime < duration) {
-            requestAnimationFrame(scrollLoop);
+            requestAnimationFrame(scrollLoop); // weiter scrollen, wenn die Zeit noch nicht abgelaufen ist
         } else {
-            window.scrollTo(0, targetPosition);
+            window.scrollTo(0, targetPosition); // Scroll-Position auf das Ziel setzen
         }
     }
 
-    requestAnimationFrame(scrollLoop);
+    requestAnimationFrame(scrollLoop); // Start der Scroll-Schleife
 }
